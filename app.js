33503d76ff29592d400cea00071588e4c80f8f66
glasfisch3000 <jakob@danckwerts.net>
2023-12-01 20:42:05 +0100

    added logs

--- app.js
+++ app.js
@@ -3,37 +3,49 @@ const { log, err, childLogger } = require(__dirname + "/logging.js")(["app"])
 log("importing modules")
 const fs = require("fs").promises
 
-const express = require("express")
-const server = express()
-const http = require("http").Server(server)
+const express = require("express")()
+const http = require("http").Server(express)
 const port = 80
 
 const { loadAllAssets } = require(__dirname + "/data.js")
 
 log("configuring http server")
-server.get("/", async (req, res) => {
-  const assets = await loadAllAssets(childLogger)
-  const blocks = assets.map(asset =>
-    `<a class="asset-link" href="${asset.url}">\n` +
-    `  <div class="asset-container">\n` +
-    `    <img src="${asset.thumbnail}" class="asset-thumbnail"></img>\n` +
-    `    <div class="asset-name">${asset.name}</div>\n` +
-    `    <div class="asset-id">${asset.id}</div>\n` +
-    `  </div>\n` +
-    `</a>`
-  ).join("\n")
 
-  let file = new String(await fs.readFile(__dirname + "/client/index.html"))
-  file = file.replace("<!--$content-->", blocks)
+const server = childLogger("server")
+express.get("/", async (req, res) => {
+  server.log("http get request: /")
 
-  res.send(file)
+  try {
+    const assets = await loadAllAssets(childLogger)
+    const blocks = assets.map(asset =>
+      `<a class="asset-link" href="${asset.url}">\n` +
+      `  <div class="asset-container">\n` +
+      `    <img src="${asset.thumbnail}" class="asset-thumbnail"></img>\n` +
+      `    <div class="asset-name">${asset.name}</div>\n` +
+      `    <div class="asset-id">${asset.id}</div>\n` +
+      `  </div>\n` +
+      `</a>`
+    ).join("\n")
+
+    let file = new String(await fs.readFile(__dirname + "/client/index.html"))
+    file = file.replace("<!--$content-->", blocks)
+
+    res.send(file)
+  } catch(error) {
+    server.err(error)
+  }
 })
 
-server.get("/style", (req, res) => {
-  res.sendFile(__dirname + "/client/main.css")
+express.get("/style", (req, res) => {
+  server.log("http get request: /style")
+  try {
+    res.sendFile(__dirname + "/client/main.css")
+  } catch(error) {
+    server.err(error)
+  }
 })
 
 log("starting http server")
 http.listen(port, () => {
-  log(`http server listening on port ${port}`)
+  server.log(`http server listening on port ${port}`)
 })
